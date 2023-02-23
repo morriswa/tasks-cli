39d08c89960b99e4575d2d58eef5686fc8bdeb7e
morriswa <william@morriswa.org>
2023-02-22 21:26:44 -0600

    back to working messageboard

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,35 @@ use cursive::{views::{Dialog, TextView, EditView}, Cursive, view::{Nameable, Res
 fn main() {
     // Creates the cursive root - required for every application.
     let mut siv = cursive::default();
-    
-    render_home(&mut siv,"Hello World!");
 
-    // Starts the event loop.    
+    let message = String::from("Hello World!");
+    
+    siv.add_layer(Dialog::around(TextView::new(message.clone()))
+        .title("Welcome")
+        .button("Dummy", move |s| {
+            rewrite_message(s,message.clone());
+        })
+        .button("Quit", |s| s.quit()));
+    
+        // Starts the event loop.    
     siv.run();
+    
 }
 
-fn render_home(s: &mut Cursive, message: &str) {
-    // let message:Option<String> = s.take_user_data();
-    // let message:String = message.unwrap();
-    
-    // Creates a dialog with a single "Quit" button
-    s .add_layer(Dialog::around(TextView::new(message
-    ))
+fn re_render_home(s: &mut Cursive, new_message: String) {
+    let message:String = String::from(new_message);
+    s.pop_layer();
+    s.add_layer(Dialog::around(TextView::new(message.clone()))
         .title("Welcome")
-        .button("Dummy", |s| {
-            s.pop_layer();
-            rewrite_message(s);
+        .button("Dummy", move |s| {
+            rewrite_message(s, message.clone());
         }
         )
-        .button("Quit", |s| s.quit()));
+        .button("Quit", |s| s.quit())); 
 }
 
-
-fn rewrite_message(s: &mut Cursive) {
-    fn ok(s: &mut Cursive, name: &str) {
-        s.set_user_data(name.to_string());
-        s.pop_layer();
-        render_home(s, name);
-    }
-
-    fn bye(s: &mut Cursive) {
-        s.set_user_data("Default".to_string());
-        s.pop_layer();
-        render_home(s, "Hello Default!");
-    }
-
+fn rewrite_message(s: &mut Cursive,old_msg:String) {
+    s.pop_layer();
     s.add_layer(Dialog::around(EditView::new()
             .with_name("name")
             .fixed_width(20))
@@ -51,8 +43,9 @@ fn rewrite_message(s: &mut Cursive) {
                 s.call_on_name("name", |view: &mut EditView| {
                     view.get_content()
                 }).unwrap();
-            ok(s, &name);
+                re_render_home(s,name.to_string());
         })
-        .button("Cancel", |s| {bye(s)}));    
-
+        .button("Cancel", move |s| {
+            re_render_home(s,old_msg.to_string());
+        }));    
 }
