f7f3ea87f8ed567c8c038ce6c6af0d0dcd4117ee
morriswa <william@morriswa.org>
2023-02-23 15:13:03 -0600

    did some tests, switching to dev branch

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::cmp::Ordering;
+
 use cursive::{
     Cursive, 
     view::{
@@ -6,8 +8,10 @@ use cursive::{
     },
     views::{
         Dialog, TextView, EditView, LinearLayout
-    }, 
+    }, align::HAlign, 
 };
+use cursive_table_view::{TableView, TableViewItem};
+use rand::Rng;
 
 /*
     Author:         William A. Morris
@@ -19,6 +23,42 @@ use cursive::{
 */
 
 const DATASTORE: (&str, &str) = (".wma/","tasks.json");
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+enum BasicColumn {
+    Name,
+    Count,
+    Rate,
+}
+
+#[derive(Clone, Debug)]
+struct Foo {
+    name: String,
+    count: usize,
+    rate: usize,
+}
+
+impl TableViewItem<BasicColumn> for Foo {
+    fn to_column(&self, column: BasicColumn) -> String {
+        match column {
+            BasicColumn::Name => self.name.to_string(),
+            BasicColumn::Count => format!("{}", self.count),
+            BasicColumn::Rate => format!("{}", self.rate),
+        }
+    }
+
+    fn cmp(&self, other: &Self, column: BasicColumn) -> Ordering
+    where
+        Self: Sized,
+    {
+        match column {
+            BasicColumn::Name => self.name.cmp(&other.name),
+            BasicColumn::Count => self.count.cmp(&other.count),
+            BasicColumn::Rate => self.rate.cmp(&other.rate),
+        }
+    }
+}
+
 
 fn main() {
     // Creates the cursive root - required for every application.
@@ -63,7 +103,9 @@ fn render_home(s: &mut Cursive, tasks: Vec<String>) {
         }) 
         .button("Quit", move |s| {
             print_final_list(s, tasks.clone());
-        })); 
+        })
+        // .button("Dev Table", render_dummy_table)
+    ); 
 }
 
 fn print_final_list(s: &mut Cursive,tasks: Vec<String>) {
@@ -134,6 +176,36 @@ fn del_task(s: &mut Cursive, tasks: Vec<String>) {
         .button("Cancel", move |s| {
             render_home(s,tasks.clone());
         }));     
+}
+
+fn render_dummy_table(s: &mut Cursive) {
+    let mut items = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for i in 0..50 {
+        items.push(Foo {
+            name: format!("Name {}", i),
+            count: rng.gen_range(0..=255),
+            rate: rng.gen_range(0..=255),
+        });
+    }
+    
+    let table = TableView::<Foo, BasicColumn>::new()
+        .column(BasicColumn::Name, "Name", |c| c.width_percent(20))
+        .column(BasicColumn::Count, "Count", |c| c.align(HAlign::Center))
+        .column(BasicColumn::Rate, "Rate", |c| {
+            c.ordering(Ordering::Greater)
+                .align(HAlign::Right)
+                .width_percent(20)
+        })
+        .items(items)
+        .min_size((50, 20));
+
+
+    s.pop_layer();
+    s.add_layer(Dialog::around(table
+    )
+        .button("Bye", |s| s.quit()));
 }
 
 fn write_to_json(vec: Vec<String>) {
